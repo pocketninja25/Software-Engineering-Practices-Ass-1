c84f603c154718ce0aa730647b2894d0b79a7f54
pocketninja25 <pocketninja25@gmail.com>
2014-12-06 18:36:07 +0000

    Before sort stuff

--- Assignment-1/useFilter/usefilter.cpp
+++ Assignment-1/useFilter/usefilter.cpp
@@ -1,11 +1,95 @@
 #include "stdafx.h"
 #include "usefilter.h"
 
+
 int Process(Measurement measurements[], int size, TCHAR* filterName, TCHAR* parameter)
 {
+	HINSTANCE hinstLib;
+	Filter FilterProc;			//Function pointer to filter function
+	BOOL fFreeResult, fRunTimeLinkSuccess, fLibFound = FALSE;
+	int errorCode = Success;
 
-	HINSTANCE h;
+	//Get handle to DLL
+	hinstLib = LoadLibrary(filterName);
 
-	h = LoadLibrary(filterName);
-	return 67576486;
+	//Try to get function address
+
+	if (hinstLib != NULL)	//Handle is valid?
+	{
+		fLibFound = TRUE;
+		FilterProc = (Filter)GetProcAddress(hinstLib, "filter");
+
+		//try calling the function
+		if (FilterProc != NULL)	//Function address is valid?
+		{
+			fRunTimeLinkSuccess = TRUE;
+			errorCode = (FilterProc)(measurements, size, parameter);	//Attempt to Apply filter and take error code for returning
+		}
+		//Free the DLL
+		fFreeResult = FreeLibrary(hinstLib);
+	}
+
+
+	//Error code returns
+
+	if (!fRunTimeLinkSuccess)
+	{
+		//could not link to functions
+		errorCode = FilterInvalidFormat;
+
+	}
+	if (!fFreeResult)
+	{
+		//Could not unload dll
+		errorCode = UnknownError;
+	}
+	if (!fLibFound)
+	{
+		//Could not find dll
+		errorCode = FilterNotFound;
+	}
+
+
+	return errorCode;
+
 }
+
+void Swap(void* value1, void* &measurement2, int itemSize)
+{
+	char hold;
+
+	for (int i = 0; i < itemSize; i++)	//Count through bytes of data items
+	{
+		hold = *(static_cast<char*> (value1)+i));		//Convert void pointer to char pointer to perform pointer arithmetic per byte then store value at pointer address in hold variable
+		*(static_cast<char*> (value1)+i)) = *(static_cast<char*> (measurement2)+i));	//Set value at 
+		*(static_cast<char*> (measurement2)+i)) = hold;
+	}
+
+	void Sort(void* base, int num, int size, Compare compareFunc)
+	{
+		char* incBase = static_cast<char*> (base);		//Convert pointer to char so can increment the pointer's value - char is 1 byte
+		bool swapped;
+		do
+		{
+			swapped = false;	//Have values been swapped this pass
+			num--;		//Adjust the end of the sort so already sorted values are not touched 
+			//also reduces size to mark the end of the list
+
+			for (int i = 0; i < size; i++)	//Count through unsorted array
+			{
+				//If pair is out of order
+
+				//Use casting to convert base pointer to char to do mathematics to find the elements of the array based on size and position of the elements
+				if (compareFunc(static_cast<void*>(static_cast<char*>(base)+(i       * size)),	//basepointer + (arrayposition * sizeofelement) gives the address of the arrayposition-th item
+					static_cast<void*>(static_cast<char*>(base)+((i + 1) * size))
+					) > 0)																	//simplified - if (compareFunc(array[i], array[i + 1]))  > 0) //Items Need swapping
+				{
+					//Swap pair and note that a swap has occured
+					Swap(measurements[i], measurements[i + 1]);
+					swapped = true;
+				} //End of if statement
+			} //End of for loop
+
+		} while (swapped);
+
+	}	//End of sort function
--- Assignment-1/useFilter/usefilter.h
+++ Assignment-1/useFilter/usefilter.h
@@ -1,7 +1,25 @@
+#ifndef FILTERTYPES_H
+#define FILTERTYPES_H
+
 #include "stdafx.h"
-#include "filtertypes.h"
-#include <Windows.h>
-#include <stdio.h>
 #include <tchar.h>
 
-int Process(Measurement measurements[], int size, TCHAR * filterName, TCHAR * parameter);
+
+const int Success = 0;
+const int FilterNotFound = 103; // can't find dll for the filter
+const int FilterInvalidFormat = 105; // Unable to find function in the dll
+const int UnknownError = 107;
+
+const int NAMESIZE = 20; // one more than the maximum number of filter (dll) name
+const int MAXMEASUREMENTS = 1000;  // the maximum size of the array of MEASUREMENTS
+
+class Measurement {
+public:
+	double theTime;
+	double theReading;
+};
+
+typedef int(*Filter)(Measurement*, int, TCHAR*);
+typedef int(*Compare)(const void*, const void*);	//Return < 0 if p1 < p2, 0 if p1 == p2, > 0 if p1 > 0
+
+#endif
